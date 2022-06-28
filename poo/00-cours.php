4144f13bdbc8b93ed2a0aec88daf6ba09782e53b
Erwan NEDELEC <100927435+Erwan-91@users.noreply.github.com>
2022-06-28 16:26:08 +0200

    up

---
+++ poo/00-cours.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <title>Introduction - cours programmation orienté objet</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="p-5 bg-light">
+        <div class="container">
+            <h1 class="display-3">POO : La programmation orientée objet</h1>
+            <p class="lead">Introduction</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12">
+                <h2 class="text-center">Historique</h2>
+                <p>La notion d'objet a été introduite dans les languages de programmation en 1962 par le language simlula. Cette façon de programmer a évoluée avant d'être popularisée en 1983 avec la sortie du language C++, un language orienté objet dont l'utilisation ressemble à celle du language C.</p>
+            </div>
+
+            <ul>
+                <li>Elle facilite les changements important dans le programme.</li>
+                <li>En cas de changements dans les languages elle facilite la mise à niveau.</li>
+            </ul>
+
+            <p>La POO va permettre de faire face aux changements majeurs qui auraient pus demander de reprendre un programme de A à Z. Mais elle va demander également une nouvelle façon de programmer et de réfléchir à l'organisation du code. 3 stragégies sont alors à mettre en place</p>
+
+
+            <ul>
+                <li>Modéliser différemment</li>
+                <li>Modulariser</li>
+                <li>Encapsuler</li>
+            </ul>
+        </div>
+        <!-- Fin col-12 -->
+        <div class="col-12">
+            <h2 class="text-center">Les avantages de la PDO</h2>
+            <p>Le principal avantage de la PDO est de faciliter la réutilisation d'un module. Ainsi, on pourra reprendre les méthodes (fonctions) d'une classe existante dans une nouvelle classe grâce à la notion d'héritage.</p>
+            <p>Une autre chose facilitée par l'orienté objet est la lecture du code. En effet on comprendra plus facilement une instruction <code>->ajout</code>, qu'un if else avec de multiples conditions dans une boucle, la force de cette façon de programmer est qu'elle se calque sur la réalité physique. Une classe sera par exemple Renaul, les objets ou instanciations de cette classe seront $voiture1, $voiture2, etc.. L'objet est donc la voiture. Cet objet aura des caractéristiques représentées par des variables, appelées propriétés, et des comportements représentés par des fonctions appelées méthodes. En conclusion, les avantages de cette façon de programmer sont les suivants :</p>
+
+            <ul>
+                <li>La facilitation du travail collaboratif</li>
+
+                <li>La simplification de la maintenance</li>
+
+                <li>L'assouplissement du code</li>
+            </ul>
+            <p>Cette approche est tout de même moin intuitive que l'approche procédurale. La POO oblige en effet à réfléchir et modéliser avant de programmer</p>
+        </div>
+        <!-- fin col 12 -->
+    </div>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ poo/01-objet.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <title>Introduction - cours programmation orienté objet</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="p-5 bg-light">
+        <div class="container">
+            <h1 class="display-3">POO : La programmation orientée objet</h1>
+            <p class="lead">Introduction</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12">
+                <h2 class="text-center">Que sont les objets ?</h2>
+                <p>Les objets ressemblent fortement aux variables que l'on connaît (string. integer. float. array. etc..) mais ces dernières sont limitées et on peut se retrouver bloqué. Par exemple lorsque l'on crée un reseau social et que l'on veut sauvegarder un utilisateur. A ce moment là on voudra définir ses limites, ses droits etc.. Ces tableaux deviennent vite très complexes et si on doit transmettre notre code, les personnes qui ne l'ont pas écrit s'y perdent facilement. Un autre exemple où le procédural est compliqué à appliquer est lorsque l'ont s'occupe des dates. Elles sont en effet considérées comme des chaînes de caractères et faire des ajouts de jours ou de mois dessus devient très compliqué. Il faudra convertir les données et passé d'un format à l'autre. En orienté objet, les manipulations de dates seront beaucoup plus simples. </p>
+                <p class="alert alert-secondary">
+                    <strong>MaDate</strong> est une classe <br><strong>$date1 = nex MaDate;</strong> sont des objets ou bien des instanciations de cette classe.
+                </p>
+            </div>
+            <!-- fin col 12 -->
+            <div class="col-12">
+                <h2 class="text-center">Les propriétés</h2>
+                <p>Sur nos différents onjets (ou instanciations) nous allons avoir des propriétés mais aussi plusieurs méthodes : sur une classe maDate on pourra imaginer des propriétés pour afficher le nombre de jours mais aussi des méthodes pour ajouter des jours ou des mois </p>
+            </div>
+            <!-- fin col 12 -->
+            <div class="col-12">
+                <h2 class="text-center">Les méthodes</h2>
+                <p>Comme nous en avons parlé au dessus, les classes peuvent aussi contenir des méthodes. Ces dernières sont en effet des fonctions, appelées méthode dans le cadre de la POO. Elles ont donc la même nomenclature qu'en procédural : <code>nomMethode();</code></p>
+            </div>
+           
+        </div>
+        
+    </div>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+
+</html>
